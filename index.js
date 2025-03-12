a2a5d752ad7b9bfb700efa6a014d596f50ec2763
Juninho <jonildopereira5@gmail.com>
2025-03-12 14:22:39 -0300

    Added a user entity with login and register functions with authentication

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const carRoutes = require("./router/carRouter.js")
 const docRoutes = require("./router/docRouter.js")
 const manufacturerRoutes = require('./router/manufacturerRouter.js')
 const accessoryRoutes = require('./router/accessoryRouter.js')
+const loginRoutes = require('./router/userRouter.js')
 const db = require("./db/db.js")
 
 const app = express()
@@ -12,6 +13,7 @@ app.use("/api", carRoutes)
 app.use("/api", docRoutes)
 app.use("/api", manufacturerRoutes)
 app.use("/api", accessoryRoutes)
+app.use("/api", loginRoutes)
 
 const port = 3000
 app.listen(port, () => {
